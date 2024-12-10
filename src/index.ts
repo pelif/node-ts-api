5dd2c20cee3123a6f0e8c239124b742f8acccec3
Felipe Dani <fdaniel@webeleven.com.br>
2024-12-10 09:02:06 -0300

    implementing cors and access control allow

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,23 @@ import bodyParser from "body-parser";
 import ProductEndpoints from "./routes/product";
 import CategoryEndpoints from "./routes/category";
 import connectDb from "./models";
+import cors from "cors";
 
 const app = express();
 const port = 3005; 
 
-
-
 app.use(express.json());
-
-connectDb().then(() => { 
-    console.log('Conectado ao banco de dados');   
-})
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
+
+app.use(cors());
+
+connectDb().then(() => { 
+    console.log('Conectado ao banco de dados');   
+})
 
 app.use('/products', ProductEndpoints); 
 app.use('/categories', CategoryEndpoints); 
